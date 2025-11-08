c5b761577792ca60b87d924ca42add4d377982d9
Mahfuz <muhammadmahfuz66412@gmail.com>
2025-11-08 11:38:14 +0600

    reports

--- 04 Fourth Semester/CSE 2204_Computer Algorithms Lab/2204 lab 6/LCS.cpp
+++ 04 Fourth Semester/CSE 2204_Computer Algorithms Lab/2204 lab 6/LCS.cpp
@@ -1,20 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    string a, b;
-    getline(cin, a);
-    getline(cin, b);
-
-    int n = a.size(), m = b.size();
-
-    cout << lcslength(a, b, n, m) << endl;
-
-    return 0;
+void lcs(string a, string b,int n,int m, vector<vector<int>>& matrix){
+    string lcs_str = "";
+    while(n > 0 && m > 0){
+        if(a[n-1] == b[m-1]){
+            lcs_str = a[n-1] + lcs_str;
+            n--;
+            m--;
+        }else if(matrix[n-1][m] > matrix[n][m-1]){
+            n--;
+        }else{
+            m--;
+        }
+    }
+    cout << "LCS: " << lcs_str << endl;
 }
 
-int lcslength(string a, string b,int n,int m){
-    vector<vector<int>> matrix(n + 1, vector<int>(m + 1, 0));
+int lcslength(string a, string b,int n,int m, vector<vector<int>>& matrix){
     for(int i=1;i<=n;i++){
         for(int j=1;j<=m;j++){
             if(a[i-1]==b[j-1]){
@@ -24,7 +27,19 @@ int lcslength(string a, string b,int n,int m){
             }
         }
     }
+    lcs(a, b, n, m, matrix);
     return matrix[n][m];
 }
 
-int lcs(string a, string b,int n,int m){
+int main(){
+    string a, b;
+    getline(cin, a);
+    getline(cin, b);
+
+    int n = a.size(), m = b.size();
+    vector<vector<int>> matrix(n + 1, vector<int>(m + 1, 0));
+
+    cout << lcslength(a, b, n, m, matrix) << endl;
+
+    return 0;
+}
